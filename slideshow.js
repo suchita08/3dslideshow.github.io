7ff8d19843f80e30b7754fc60ef6de5a10a48543
Suchita <suchitayadav73@gmail.com>
2020-06-01 15:26:09 +0530

    Slide Show

---
+++ slideshow.js
@@ -0,0 +1,20 @@
+window.onload= function(){
+    document.querySelector('.banner button').addEventListener('click', () => {
+        document.querySelector('.banner').style.display = 'none';
+        document.querySelector('.slideshow-wrapper').style.cssText = 'opacity: 1; visibility: visible';
+        document.querySelector('.slideshow').style.animation = 'slideshow 24s infinite';
+    });
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
